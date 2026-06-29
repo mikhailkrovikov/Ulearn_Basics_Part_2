af0ffdcea9e4a1bcaf1d05d17594990a3e605801
Михаил Кровиков <mikhailkrovikov@gmail.com>
2026-06-29 15:23:55 +0300

    average task

--- Module_2_Yield_Return/Smooth/MovingAverageTask.cs
+++ Module_2_Yield_Return/Smooth/MovingAverageTask.cs
@@ -1,12 +1,23 @@
 using System.Collections.Generic;
-
 namespace yield;
 
 public static class MovingAverageTask
 {
-	public static IEnumerable<DataPoint> MovingAverage(this IEnumerable<DataPoint> data, int windowWidth)
-	{
-		//Fix me!
-		return data;
-	}
+    public static IEnumerable<DataPoint> MovingAverage(this IEnumerable<DataPoint> data, int windowWidth)
+    {
+        DataPoint avGpoint = null;
+        var queueForAvg = new Queue<double>();
+        double sum = 0;
+        double avG = 0;
+        foreach (DataPoint point in data)
+        {
+            queueForAvg.Enqueue(point.OriginalY);
+            if (queueForAvg.Count > windowWidth)
+                sum -= queueForAvg.Dequeue();
+            sum += point.OriginalY;
+            avG = sum / queueForAvg.Count;
+            avGpoint = point.WithAvgSmoothedY(avG);
+            yield return avGpoint;
+        }
+    }
 }
