a80aea0d0f6e9f29952c6bae5c75e17757a4f15e
Михаил Кровиков <mikhailkrovikov@gmail.com>
2026-06-29 15:31:11 +0300

    bf commands

--- Module_4_Delegates/brainfuck/BrainfuckBasicCommands.cs
+++ Module_4_Delegates/brainfuck/BrainfuckBasicCommands.cs
@@ -1,17 +1,58 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Runtime.CompilerServices;
 
 namespace func.brainfuck
 {
-	public class BrainfuckBasicCommands
-	{
-		public static void RegisterTo(IVirtualMachine vm, Func<int> read, Action<char> write)
-		{
-			vm.RegisterCommand('.', b => { });
-			vm.RegisterCommand('+', b => {});
-			vm.RegisterCommand('-', b => {});
-			//...
-		}
-	}
+    public class BrainfuckBasicCommands
+    {
+        public static void RegisterTo(IVirtualMachine vm, Func<int> read, Action<char> write)
+        {
+            string validChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            vm.RegisterCommand('.', b =>
+            {
+                write((char)b.Memory[b.MemoryPointer]);
+            });
+
+            vm.RegisterCommand('+', b =>
+            {
+                if (b.Memory[b.MemoryPointer] == 255)
+                    b.Memory[b.MemoryPointer] = 0;
+                else
+                    b.Memory[b.MemoryPointer]++;
+            });
+
+            vm.RegisterCommand('-', b =>
+            {
+                if (b.Memory[b.MemoryPointer] == 0)
+                    b.Memory[b.MemoryPointer] = 255;
+                else b.Memory[b.MemoryPointer]--;
+            });
+
+            vm.RegisterCommand(',', b =>
+            {
+                b.Memory[b.MemoryPointer] = (byte)read();
+            });
+
+            vm.RegisterCommand('>', b =>
+            {
+                if (b.MemoryPointer == b.Memory.Length - 1)
+                    b.MemoryPointer = 0;
+                else b.MemoryPointer++;
+            });
+
+            vm.RegisterCommand('<', b =>
+            {
+                if (b.MemoryPointer == 0)
+                    b.MemoryPointer = b.Memory.Length - 1;
+                else b.MemoryPointer--;
+            });
+            foreach (var c in validChars)
+            {
+                vm.RegisterCommand(c, b =>
+                {
+                    b.Memory[b.MemoryPointer] = (byte)c;
+                });
+            }
+        }
+    }
 }
