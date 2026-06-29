0e1cc31b49f027561ea64248420c5bbab179ad28
Михаил Кровиков <mikhailkrovikov@gmail.com>
2026-06-29 15:24:16 +0300

    max task

--- Module_2_Yield_Return/Smooth/MovingMaxTask.cs
+++ Module_2_Yield_Return/Smooth/MovingMaxTask.cs
@@ -1,14 +1,32 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 
 namespace yield;
 
 public static class MovingMaxTask
 {
-	public static IEnumerable<DataPoint> MovingMax(this IEnumerable<DataPoint> data, int windowWidth)
-	{
-		//Fix me!
-		return data;
-	}
+    public static IEnumerable<DataPoint> MovingMax(this IEnumerable<DataPoint> data, int windowWidth)
+    {
+        DataPoint maxPoint = null;
+        Queue<double> queue = new Queue<double>();
+        LinkedList<double> linkedList = new LinkedList<double>();
+        foreach (DataPoint point in data)
+        {
+            while (linkedList.Count > 0 && linkedList.Last.Value < point.OriginalY)
+            {
+                linkedList.RemoveLast();
+            }
+            linkedList.AddLast(point.OriginalY);
+            queue.Enqueue(point.OriginalY);
+            if (queue.Count > windowWidth)
+            {
+                if (linkedList.First.Value == queue.Dequeue())
+                    linkedList.RemoveFirst();
+            }
+            maxPoint = point.WithMaxY(linkedList.First.Value);
+            yield return maxPoint;
+        }
+    }
 }
