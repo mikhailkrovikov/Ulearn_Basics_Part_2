d5f27787e6ac6d1d2a16f59badc21a2deac6ed5a
Михаил Кровиков <mikhailkrovikov@gmail.com>
2026-06-29 15:14:28 +0300

    limited size stack task

---
+++ Module_1_Generics/LimitedSizeStack/LimitedSizeStack.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace LimitedSizeStack;
+
+public class LimitedSizeStack<T>
+{
+    LinkedList<T> limitedSizeStack = new LinkedList<T>();
+    public int UndoLimit { get; set; }
+    public LimitedSizeStack(int undoLimit)
+    {
+        UndoLimit = undoLimit;
+    }
+
+    public void Push(T item)
+    {
+        limitedSizeStack.AddLast(item);
+        if (Count > UndoLimit)
+            limitedSizeStack.RemoveFirst();
+    }
+
+    public T Pop()
+    {
+        var a = limitedSizeStack.Last.Value;
+        limitedSizeStack.RemoveLast();
+        return a;
+    }
+
+    public int Count => limitedSizeStack.Count;
+}
