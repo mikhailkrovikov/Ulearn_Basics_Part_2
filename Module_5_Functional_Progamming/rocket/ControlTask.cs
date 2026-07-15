523304b9700becfe909d0a27354124d068ee4c18
Михаил Кровиков <mikhailkrovikov@gmail.com>
2026-07-15 08:56:23 +0300

    control task

--- Module_5_Functional_Progamming/rocket/ControlTask.cs
+++ Module_5_Functional_Progamming/rocket/ControlTask.cs
@@ -1,9 +1,16 @@
+using System;
+
 namespace func_rocket;
 
 public class ControlTask
 {
-	public static Turn ControlRocket(Rocket rocket, Vector target)
-	{
-		return Turn.None;
-	}
+    public static Turn ControlRocket(Rocket rocket, Vector target)
+    {
+        var vector = target - rocket.Location;
+        var angle = (rocket.Direction - vector.Angle) / 2 + (rocket.Velocity.Angle - vector.Angle);
+        if (Math.Abs(angle) > 0)
+            if (angle > 0) return Turn.Left;
+            else return Turn.Right;
+        return Turn.None;
+    }
 }
